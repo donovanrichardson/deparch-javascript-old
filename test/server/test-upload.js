5669c707226875c42ca4f576caa600c1831a116c
donovanrichardson <donovanspqa@gmail.com>
2020-04-10 15:19:58 -0400

    abandoning this route

--- test/server/test-upload.js
+++ test/server/test-upload.js
@@ -18,28 +18,82 @@ describe("example-zip", function(){
 
 describe("insertions", function(){
 
-    afterEach(async () => {
+    after(async () => {
         await knex.destroy()
       })
 
-    it('inserts', (done) =>{
-        knex('feed').insert({id: 'exam', title:'Example Fuid', location:0, latest:'nunyabeezwax'},'*').then( function(r){
-            const theid = r[0]['id'];
-            expect(theid).to.equal('exam');
-            done();
-        }).catch((err) => {
-            console.error(err);
-            done();
-        })
+/*     it('inserts', (done) =>{
+        const theTest = () =>{
+            knex('feed').insert({id: 'exam', title:'Example Fuid', location:0, latest:'nunyabeezwax'},'*').then( function(r){
+                return r[0]['id'];
+                done();
+            }).catch((err) => {
+                done();
+            })
+        }
+
+        expect(theTest()).to.equal("exam")
+        
     })
 
     it('deletes', (done) => {
         knex('feed').where('id', 'exam').del().then( (removed) => {
-            console.log(removed);
+            expect(removed).to.equal(2)
             done();
         }).catch((err) => {
             console.error(err);
             done();
         })
-    })
+    }) */
 });
+
+describe("scope", async () => {
+    it('does the thing', async() =>{
+
+    const nunya = async () =>{
+        return await knex('feed').insert({id: 'exam', title:'Example Fuid', location:0, latest:'nunyabeezwax'},'*').rows
+    }
+
+    try {
+        const bees = await nunya();
+        expect(typeof(bees)).to.equal('');
+    }catch (e){
+        console.error(e);
+    }
+
+    // console.log(knex('feed').insert({id: 'exam', title:'Example Fuid', location:0, latest:'nunyabeezwax'},'*'));
+
+    // after(async () => {
+    //     await knex.destroy()
+    //   })
+
+    // const theId = async () =>{
+    //     await knex('feed').insert({id: 'exam', title:'Example Fuid', location:0, latest:'nunyabeezwax'},'*').then( function(r){
+    //         // console.log(r[0]['id']);
+    //         return r[0]['id'];
+    //     }).catch((err) => {
+    //         console.error(err);
+    //     })
+    // }
+
+    // const howManyDeleted = async () => {
+    //         await knex('feed').where('id', 'exam').del().then( (removed) => {
+    //             return removed;
+    //         }).catch((err) => {
+    //             console.error(err);
+    //     })
+    // }
+
+    // console.log(typeof(theId()));
+    // console.log(typeof(howManyDeleted()));
+
+    // expect(theId()).to.equal('1');
+
+    // it("is 4", () =>{
+    //     let answer = () =>{
+    //         return 2 + 2
+    //     }
+    //     expect(answer()).to.equal(5);
+    // })
+})
+})
